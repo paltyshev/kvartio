c1bf629394232cb16ed7794e56592926bc02c602
paltyshev <aleksep@gmail.com>
2023-07-01 17:47:26 +0300

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,29 @@
+import Image from 'next/image';
+import { Inter } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export default function Home() {
+  return (
+    <div
+      className="hero min-h-screen"
+      style={{
+        backgroundImage:
+          'url(/remont-kvartir-v-gelendzhike.webp)',
+      }}
+    >
+      <div className="hero-overlay bg-opacity-50"></div>
+      <div className="hero-content text-center text-neutral-content">
+        <div className="max-w-md">
+          <h1 className="mb-5 text-5xl font-bold">Ремонт квартир в Геленджике</h1>
+          <p className="mb-5">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
+            a id nisi.
+          </p>
+          <button className="btn btn-primary">Get Started</button>
+        </div>
+      </div>
+    </div>
+  );
+}
